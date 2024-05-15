22261391b2f75f743c5b1bb357d0e145d2ab281d
kim-jong-young <kim-jong-young@localhost>
2024-05-15 21:53:15 +0900

    test: 회원 도메인 테스트 코드 추가

---
+++ msa-board-domains/msa-board-doamin-member/src/test/java/com/jong/msa/board/domain/member/test/MemberDomainTest.java
@@ -0,0 +1,56 @@
+package com.jong.msa.board.domain.member.test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.jong.msa.board.common.enums.DBCodeEnum.Gender;
+import com.jong.msa.board.common.enums.DBCodeEnum.Group;
+import com.jong.msa.board.domain.member.entity.MemberEntity;
+import com.jong.msa.board.domain.member.repository.MemberRepository;
+
+@DataJpaTest
+@ContextConfiguration(
+		initializers = ConfigDataApplicationContextInitializer.class, 
+		classes = MemberDomainTestContext.class)
+public class MemberDomainTest {
+
+	@Autowired
+	MemberRepository repository;
+	
+	@Test
+	void contextLoad() {
+		
+		System.out.println("context load.");
+	}
+	
+	@Test
+	void 회원계정_Unique_컬럼_테스트() {
+		
+		repository.save(MemberEntity.builder()
+				.username("username")
+				.password("password")
+				.name("name")
+				.gender(Gender.MAIL)
+				.email("email")
+				.group(Group.ADMIN)
+				.build());
+		
+		repository.save(MemberEntity.builder()
+				.username("username")
+				.password("password")
+				.name("name")
+				.gender(Gender.MAIL)
+				.email("email")
+				.group(Group.ADMIN)
+				.build());
+
+		assertThrows(DataIntegrityViolationException.class, repository::flush);
+	}
+	
+}
---
+++ msa-board-domains/msa-board-doamin-member/src/test/java/com/jong/msa/board/domain/member/test/MemberDomainTestContext.java
@@ -0,0 +1,10 @@
+package com.jong.msa.board.domain.member.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.jong.msa.board")
+public class MemberDomainTestContext {
+
+}
