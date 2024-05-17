d6b94f16be9eef9e761d60c5021bbeb86d3f4242
Kim Jong Young <jykim@saltlux.com>
2024-05-17 20:06:35 +0900

    refactor: 불필요한 import 문 삭제

--- msa-board-cores/msa-board-core-persist/src/main/java/com/jong/msa/board/core/persist/config/PersistCoreConfig.java
+++ msa-board-cores/msa-board-core-persist/src/main/java/com/jong/msa/board/core/persist/config/PersistCoreConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zaxxer.hikari.HikariConfig;
