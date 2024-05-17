6f08746c17d7eaca7250968a97ec8a5e6d035848
Kim Jong Young <jykim@saltlux.com>
2024-05-17 20:58:26 +0900

    test: Security 모듈 테스트 코드 추가

---
+++ msa-board-cores/msa-board-core-security/src/test/java/com/jong/msa/board/core/security/test/SecurityCoreTestContext.java
@@ -0,0 +1,54 @@
+package com.jong.msa.board.core.security.test;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import redis.embedded.RedisExecProvider;
+import redis.embedded.RedisServer;
+import redis.embedded.util.Architecture;
+import redis.embedded.util.OS;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.jong.msa.board")
+public class SecurityCoreTestContext {
+
+	private RedisServer embeddedRedisServer;
+
+	@Value("${spring.redis.port}") 
+	private int redisPort;
+
+	@PostConstruct
+	public void start() throws Exception {
+
+		(embeddedRedisServer = RedisServer.builder()
+				.redisExecProvider(RedisExecProvider.defaultProvider()
+						.override(OS.MAC_OS_X, Architecture.x86_64, "binary/redis-server-6.2.5-mac-arm64")
+						.override(OS.MAC_OS_X, Architecture.x86, "binary/redis-server-6.2.5-mac-arm64"))
+				.port(redisPort)
+				.build()).start();
+	}
+
+	@PreDestroy
+	public void end() throws Exception {
+		
+		if (embeddedRedisServer != null && embeddedRedisServer.isActive()) {
+			embeddedRedisServer.stop();
+		}
+	}
+
+	@Bean
+	ObjectMapper objectMapper() {
+		
+		return new ObjectMapper();
+	}
+
+}
---
+++ msa-board-cores/msa-board-core-security/src/test/java/com/jong/msa/board/core/security/test/annotation/SecurityCoreTestUser.java
@@ -0,0 +1,47 @@
+package com.jong.msa.board.core.security.test.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import com.jong.msa.board.common.enums.DBCodeEnum.Group;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = SecurityCoreTestUser.ContextFactory.class)
+public @interface SecurityCoreTestUser {
+
+	String id();
+		
+	Group[] groups();
+	
+	public static class ContextFactory implements WithSecurityContextFactory<SecurityCoreTestUser> {
+
+		@Override
+		public SecurityContext createSecurityContext(SecurityCoreTestUser annotation) {
+			
+			List<GrantedAuthority> authorityList = Stream.of(annotation.groups())
+				.map(Group::name)
+				.map(String::toUpperCase)
+				.map(x -> new SimpleGrantedAuthority("ROLE_" + x))
+				.collect(Collectors.toList());
+			
+			SecurityContext context = SecurityContextHolder.createEmptyContext();
+			
+			context.setAuthentication(new UsernamePasswordAuthenticationToken(annotation.id(), null, authorityList));
+			
+			return context;
+		}
+
+	}
+	
+}
