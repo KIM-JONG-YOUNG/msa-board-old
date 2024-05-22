c840869c9b24e8d9f805e85e4b3cfab5cdd84ab9
Kim Jong Young <jykim@saltlux.com>
2024-05-22 18:05:14 +0900

    feat: 검색 정렬을 위한 상수 추가

--- msa-board-view/src/constants/Constants.tsx
+++ msa-board-view/src/constants/Constants.tsx
@@ -20,6 +20,23 @@ export const EMAIL_DOMAIN = {
 } as const
 
 export const MEMBER_SORT = {
-    NAVER_DOMAIN: "naver.com",
-    GMAIL_DOMAIN: "gmail.com"
+    USERNAME: "계정",
+    NAME: "이름",
+    EMAIL: "이메일",
+    CREATED_DATE_TIME: "생성일시",
+    UPDATED_DATE_TIME: "수정일시"
+} as const
+
+export const POST_SORT = {
+    TITLE: "제목",
+    CONTENT: "내용",
+    WRITER: "작성자 계정",
+    VIEWS: "조회수",
+    CREATED_DATE_TIME: "생성일시",
+    UPDATED_DATE_TIME: "수정일시"
+} as const
+
+export const ORDER = {
+    ASC: "오름차순",
+    DESC: "내림차순"
 } as const
