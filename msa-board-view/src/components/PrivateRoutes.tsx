c21b601d3c3fb30531e34f4cb4ef0f7a30980a16
Kim Jong Young <jykim@saltlux.com>
2024-05-22 17:54:34 +0900

    feat: 회원 별 라우터 추가

---
+++ msa-board-view/src/components/PrivateRoutes.tsx
@@ -0,0 +1,74 @@
+import { Navigate } from "react-router-dom";
+import { GROUP } from "../constants/Constants";
+
+export function PrivateAuthRouter(prop: {
+  permitGroups: Array<keyof typeof GROUP>
+  redirectURL: string
+  element: React.ReactElement,
+}): React.ReactElement {
+
+  const group = sessionStorage.getItem("Group")
+  let isAuth = false;
+
+  if (prop.permitGroups.length === 0) {
+    // 로그인이 안되어 있을 경우에만 체크  
+    isAuth = (!group);
+  } else {
+    // 로그인이 되어 있을 경우 
+    prop.permitGroups.forEach(x => isAuth = (!isAuth && x === group));
+  }
+
+  return (
+    isAuth ? prop.element : <Navigate to={prop.redirectURL} />
+  );
+};
+
+export function PrivateAdminRouter(prop: {
+  element: React.ReactElement,
+}): React.ReactElement {
+
+  return (
+    <PrivateAuthRouter 
+      permitGroups={["ADMIN"]} 
+      element={prop.element} 
+      redirectURL="/admin/login/form" />
+  );
+};
+
+export function PrivateUserRouter(prop: {
+  element: React.ReactElement,
+}): React.ReactElement {
+
+  return (
+    <PrivateAuthRouter 
+      permitGroups={["USER"]} 
+      element={prop.element} 
+      redirectURL="/user/login/form" />
+  );
+};
+
+export function PrivateMemberRouter(prop: {
+  element: React.ReactElement,
+  redirectURL: string
+}): React.ReactElement {
+
+  return (
+    <PrivateAuthRouter 
+      permitGroups={["ADMIN", "USER"]} 
+      element={prop.element} 
+      redirectURL={prop.redirectURL} />
+  );
+};
+
+export function PrivateAnonymousRouter(prop: {
+  element: React.ReactElement,
+  redirectURL: string
+}): React.ReactElement {
+
+  return (
+    <PrivateAuthRouter 
+      permitGroups={[]} 
+      element={prop.element} 
+      redirectURL={prop.redirectURL} />
+  );
+};
