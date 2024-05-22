24f0d2479ab07e9fde5a392615ba7534bd4438b6
Kim Jong Young <jykim@saltlux.com>
2024-05-22 17:52:21 +0900

    feat: 상수 추가

--- msa-board-view/src/constants/Constants.tsx
+++ msa-board-view/src/constants/Constants.tsx
@@ -1,2 +1,25 @@
 
-export type LOGIN_TYPE = "ADMIN" | "USER";
+export const GROUP = {
+    ADMIN: "관리자",
+    USER: "사용자"
+} as const
+
+export const GENDER = {
+    MAIL: "남자",
+    FEMAIL: "여자"
+} as const
+
+export const STATE = {
+    ACTIVE: "활성화",
+    DEACTIVE: "비활성화"
+} as const
+
+export const EMAIL_DOMAIN = {
+    NAVER_DOMAIN: "naver.com",
+    GMAIL_DOMAIN: "gmail.com"
+} as const
+
+export const MEMBER_SORT = {
+    NAVER_DOMAIN: "naver.com",
+    GMAIL_DOMAIN: "gmail.com"
+} as const
