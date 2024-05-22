e79c1e272cf46d11318160353203b3a4e0f688d8
Kim Jong Young <jykim@saltlux.com>
2024-05-22 17:55:12 +0900

    feat: 로그인 화면 컴포넌트 추가

--- msa-board-view/src/pages/common/LoginForm.tsx
+++ msa-board-view/src/pages/common/LoginForm.tsx
@@ -1,18 +1,21 @@
 
 import { error } from 'console';
-import { LOGIN_TYPE } from '../../constants/Constants';
+import { GROUP } from '../../constants/Constants';
 import { useForm } from "react-hook-form";
-
-interface LoginFormInputs {
+import { useEffect } from 'react';
+import { fetchData } from '../../utils/fetchUtils';
+import { adminService } from '../../services/adminService';
+ 
+interface ILoginForm {
   username: string
   password: string
 }
 
 export default function LoginForm(prop: {
-  loginType: LOGIN_TYPE
+  loginType: keyof typeof GROUP
 }) {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>({ 
+  const { register, handleSubmit, formState: { errors } } = useForm<ILoginForm>({ 
     mode: "onBlur" 
   });
   const usernameRegister = register("username", {
@@ -26,8 +29,12 @@ export default function LoginForm(prop: {
     required: "비밀번호는 비어있을 수 없습니다."
   });
 
-  const login = (loginFormData: LoginFormInputs) => {
-    console.log("Login Form Data: ", loginFormData);
+  const login = async (loginFormData: ILoginForm) => {
+
+    const result = await adminService.loginAdmin(loginFormData)
+      .catch(error => console.log("Error : ", error))
+
+    console.log('loginResult : ', result);
   }
 
   return (
@@ -37,28 +44,28 @@ export default function LoginForm(prop: {
 
         <h1 className="mb-4">
           Login
-          <span className="text-primary">{prop.loginType}</span>
+          <span className="text-primary">{ prop.loginType }</span>
         </h1>
 
         <div className="row">
           <div className="col-xl-8 mb-3">
             <label className="form-label subheading">Username</label>
-            <input type="text" className="form-control" placeholder="Username..." {...usernameRegister} />
-            {errors.username && <div className='text-danger'>{errors.username.message}</div>}
+            <input type="text" className="form-control" placeholder="Username..." { ...usernameRegister } />
+            { errors.username && <div className='text-danger'>{ errors.username.message }</div> }
           </div>
         </div>
 
         <div className="row">
           <div className="col-xl-8 mb-3">
             <label className="form-label subheading">Password</label>
-            <input type="password" className="form-control" placeholder="Password..."  {...passwordRegister} />
-            {errors.password && <div className='text-danger'>{errors.password.message}</div>}
+            <input type="password" className="form-control" placeholder="Password..."  { ...passwordRegister } />
+            { errors.password && <div className='text-danger'>{ errors.password.message }</div> }
           </div>
         </div>
 
         <div className="row">
           <div className="col-xl-8 mt-3">
-            <button type="button" className="btn btn-primary text-white w-100" onClick={handleSubmit(login)}>Login</button>
+            <button type="button" className="btn btn-primary text-white w-100" onClick={ handleSubmit(login) }>Login</button>
           </div>
         </div>
       </div>
