b72ca53fb496117d9374c6cfa226f05b4342b81b
Kim Jong Young <jykim@saltlux.com>
2024-05-23 18:55:15 +0900

    refactor: KafkaProperties 클래스를 이용하여 설정 주입

--- msa-board-cores/msa-board-core-kafka/src/main/java/com/jong/msa/board/core/kafka/config/KafkaCoreConfig.java
+++ msa-board-cores/msa-board-core-kafka/src/main/java/com/jong/msa/board/core/kafka/config/KafkaCoreConfig.java
@@ -1,14 +1,13 @@
 package com.jong.msa.board.core.kafka.config;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -28,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @EnableKafka
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaCoreConfig {
 
 	private final KafkaProducerErrorHandler producerErrorHandler;
@@ -35,17 +35,9 @@ public class KafkaCoreConfig {
 	private final KafkaConsumerErrorHandler consumerErrorHandler;
 
 	@Bean
-	ProducerFactory<String, String> producerFactory(
-			@Value("${spring.kafka.producer.bootstrap-servers}") List<String> serverList) {
-
-		Map<String, Object> propMap = new HashMap<>();
+	ProducerFactory<String, String> producerFactory(KafkaProperties properties) {
 		
-		propMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverList);	// Kafka Broker 서버 IP 및 포트
-		
-		propMap.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 60000);				// Kafka Broker 로부터 Metadata를 받기까지 최대 대기 시간 (60s) 
-		propMap.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 60000);		// Kafka Broker로 전송 실패 여부 판단을 위한 최대 대기 시간 (60s)
-		propMap.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);		// Kafka Broker로 전송 후 Metadata를 받기 위한 최대 시간 (30s)
-		propMap.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);			// 재시도 대기 시간 (1s)
+		Map<String, Object> propMap = properties.buildProducerProperties();
 		propMap.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);		// Producer exactly once 설정 
 		propMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		propMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -54,18 +46,9 @@ public class KafkaCoreConfig {
 	}
 
 	@Bean
-	ConsumerFactory<String, String> consumerFactory(
-			@Value("${spring.kafka.consumer.group-id}") String groupId,
-			@Value("${spring.kafka.consumer.bootstrap-servers}") List<String> serverList,
-			@Value("${spring.kafka.consumer.auto-offset-reset}") String autoOffsetReset) {
+	ConsumerFactory<String, String> consumerFactory(KafkaProperties properties) {
 
-		Map<String, Object> propMap = new HashMap<>();
-		
-		propMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, serverList);		// Kafka Broker 서버 IP 및 포트 
-		propMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);	// Topic 관련 Metadata 정보가 없을 경우 처리 방식 
-		propMap.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);					// Consumer 그룹 ID 
-
-		propMap.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);	// 재시도 대기 시간 (1s)
+		Map<String, Object> propMap = properties.buildConsumerProperties();
 		propMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		propMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
