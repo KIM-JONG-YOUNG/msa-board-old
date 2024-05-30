a4ca35e6c7cba773f59982300629be7c7e4af2f4
KIM JONG YOUNG <kim_jong_young@naver.com>
2024-05-31 01:58:47 +0900

    fix: 권한 체크 라우터 아규먼트 변경 및 AnonymousRouter 조건 수정

--- msa-board-views/msa-board-view-common/src/components/ProtectRouter.tsx
+++ msa-board-views/msa-board-view-common/src/components/ProtectRouter.tsx
@@ -2,25 +2,26 @@ import { Navigate } from "react-router-dom";
 import * as sessionUtils from "../utils/sessionUtils";
 import { GROUP } from "../constants/constants";
 
-export function MemberRouter(  
-    element: React.ReactElement,
-    group: keyof typeof GROUP,
+export function MemberRouter(prop: {
+    element: React.ReactElement
+    group: keyof typeof GROUP
     redirectURL: string
-): React.ReactElement {
+}): React.ReactElement {
 
     const currentAccessToken = sessionUtils.getAccessToken();
     const currentGroup = sessionUtils.getGroup();
 
-    return (!!currentAccessToken && !!currentGroup && group === currentGroup) ? element : <Navigate to={redirectURL} />
+    return (!!currentAccessToken && !!currentGroup 
+        && prop.group === currentGroup) ? prop.element : <Navigate to={ prop.redirectURL } />
 };
 
-export function AnonymousRouter(
-    element: React.ReactElement,
+export function AnonymousRouter(prop: {
+    element: React.ReactElement
     redirectURL: string
-): React.ReactElement {
+}): React.ReactElement {
 
     const currentAccessToken = sessionUtils.getAccessToken();
     const currentGroup = sessionUtils.getGroup();
 
-    return (!!currentAccessToken && !!currentGroup) ? element : <Navigate to={redirectURL} />
+    return (!currentAccessToken && !currentGroup) ? prop.element : <Navigate to={ prop.redirectURL } />
 };
