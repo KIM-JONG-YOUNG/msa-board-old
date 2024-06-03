b525464932cbb85579fa6373c0c2cc58efc07357
Kim Jong Young <jykim@saltlux.com>
2024-06-03 18:18:27 +0900

    feat: 로딩 컨텍스트 사용하기 위한 LoadingProvider 컴포넌트 추가

--- msa-board-views/msa-board-view-admin/src/App.tsx
+++ msa-board-views/msa-board-view-admin/src/App.tsx
@@ -5,22 +5,28 @@ import ErrorPage from './pages/common/ErrorPage';
 import AccountLoginForm from './pages/account/AccountLoginForm';
 import { AdminRounter, NotAdminRounter } from './components/AdminRounter';
 import AccountModifyForm from './pages/account/AccountModifyForm';
+import { LoadingProvider } from 'msa-board-view-common/src/contexts/LoadingContext';
+import MemberList from './pages/member/MemberList';
 
 export default function App() {
 
     return (
         <BrowserRouter>
-            <div className="container-fluid p-0">
-                <Navigation />
-                <ErrorBoundary FallbackComponent={ErrorPage}>
-                    <Routes>
-                        <Route path="/" element={<Navigate to="/main" />} />
-                        <Route path="/main" element={<Navigate to="/account/modify/form" />} />
-                        <Route path="/account/login/form" element={<NotAdminRounter element={<AccountLoginForm />} />} />
-                        <Route path="/account/modify/form" element={<AdminRounter element={<AccountModifyForm />} />} />
-                    </Routes>
-                </ErrorBoundary>
-            </div>
+            <LoadingProvider>
+                <div className="container-fluid p-0">
+                    <Navigation />
+                    <ErrorBoundary FallbackComponent={ErrorPage}>
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/main" />} />
+                            <Route path="/main" element={<Navigate to="/member/list" />} />
+                            <Route path="/account/login/form" element={<NotAdminRounter element={<AccountLoginForm />} />} />
+                            <Route path="/account/modify/form" element={<AdminRounter element={<AccountModifyForm />} />} />
+                            <Route path="/member/list" element={<AdminRounter element={<MemberList />} />} />
+                            <Route path="/member/details" element={<AdminRounter element={<MemberList />} />} />
+                        </Routes>
+                    </ErrorBoundary>
+                </div>
+            </LoadingProvider>
         </BrowserRouter>
     );
 }
