a0c57ad596626ad49d1076a007d19239bbc3b5cd
KIM JONG YOUNG <kim_jong_young@naver.com>
2024-06-04 01:00:58 +0900

    feat: DatePicker 추가

--- msa-board-views/msa-board-view-admin/src/pages/member/MemberList.tsx
+++ msa-board-views/msa-board-view-admin/src/pages/member/MemberList.tsx
@@ -1,7 +1,7 @@
-import { useForm } from "react-hook-form";
+import { Control, Controller, useController, useForm } from "react-hook-form";
 import { useErrorBoundary } from "react-error-boundary";
-import { ERROR_CODE, GENDER, GROUP, MEMBER_SORT, ORDER, STATE } from "msa-board-view-common/src/constants/constants";
-import { useEffect, useState } from "react";
+import { DATE_TIME_FORMAT, ERROR_CODE, GENDER, GROUP, MEMBER_SORT, ORDER, STATE } from "msa-board-view-common/src/constants/constants";
+import { useEffect, useRef, useState } from "react";
 import { Modal } from "react-bootstrap";
 import usePaging from "msa-board-view-common/src/hooks/usePaging";
 
@@ -9,16 +9,20 @@ import * as adminService from "../../services/adminService";
 import { FetchErrorDetails, FetchErrorResponse } from "msa-board-view-common/src/utils/fetchUtils";
 import useLoading from "msa-board-view-common/src/hooks/useLoading";
 import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import { format } from 'date-fns';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export type MemberListSearchFormInputs = {
 	readonly username?: string
 	readonly name?: string
 	readonly gender?: keyof typeof GENDER
 	readonly email?: string
-	readonly createdDateFrom?: string
-	readonly createdDateTo?: string
-	readonly updatedDateFrom?: string
-	readonly updatedDateTo?: string
+	readonly createdDateFrom?: Date
+	readonly createdDateTo?: Date
+	readonly updatedDateFrom?: Date
+	readonly updatedDateTo?: Date
 	readonly group?: keyof typeof GROUP
 	readonly state?: keyof typeof STATE
 	readonly offset?: number
@@ -42,7 +46,9 @@ export default function MemberList() {
 	const {
 		register,
 		setError,
-		getValues	
+		getValues,
+		formState: { errors },
+		control
 	} = useForm<MemberListSearchFormInputs>({ mode: "onBlur" });
 	const { showBoundary } = useErrorBoundary();
 	const [, setLoading] = useLoading();
@@ -67,21 +73,40 @@ export default function MemberList() {
 	const nameRegister = register("name");
 	const genderRegister = register("gender");
 	const emailRegister = register("email");
-	const createdDateFromRegister = register("createdDateFrom");
-	const createdDateToRegister = register("createdDateTo");
-	const updatedDateFromRegister = register("updatedDateFrom");
-	const updatedDateToRegister = register("updatedDateTo");
 	const groupRegister = register("group");
 	const stateRegister = register("state");
 	const sortRegister = register("sort");
 	const orderRegister = register("order");
 
+	const { field: createdDateFromField } = useController({
+		control: control,
+		name: "createdDateFrom"
+	})
+	const { field: createdDateToField } = useController({
+		control: control,
+		name: "createdDateTo"
+	})
+	const { field: updatedDateFromField } = useController({
+		control: control,
+		name: "updatedDateFrom"
+	})
+	const { field: updatedDateToField } = useController({
+		control: control,
+		name: "updatedDateTo"
+	})
+
 	const search = () => {
+
+		const formData = getValues();
 
 		setLoading(true);
 
 		adminService.searchMemberList({
-			...getValues(),
+			...formData,
+			createdDateFrom: (!!formData.createdDateFrom) ? format(formData.createdDateFrom, DATE_TIME_FORMAT.DATE) : "",
+			createdDateTo: (!!formData.createdDateTo) ? format(formData.createdDateTo, DATE_TIME_FORMAT.DATE) : "",
+			updatedDateFrom: (!!formData.updatedDateFrom) ? format(formData.updatedDateFrom, DATE_TIME_FORMAT.DATE) : "",
+			updatedDateTo: (!!formData.updatedDateTo) ? format(formData.updatedDateTo, DATE_TIME_FORMAT.DATE) : "",
 			offset: (page > 0) ? (page - 1) * pageRows : 0,
 			limit: (page > 0) ? page * pageRows : pageRows
 		}).then(async (response: Response) => {
@@ -124,13 +149,9 @@ export default function MemberList() {
 		}).catch(showBoundary).finally(() => setLoading(false));
 	};
 
-	const searchPaging = (page: number) => {
-		setPage(page);
-		search();
-	};
-
 	useEffect(() => {
-		searchPaging(1);
+		setPage(1);
+		search();
 	}, []);
 
 	return (
@@ -180,26 +201,35 @@ export default function MemberList() {
 						<ul className="pagination justify-content-center">
 							{
 								(startPage > pageGroupSize) &&
-									<li className="page-item" onClick={() => searchPaging(startPage - 1)}>
-										<a className="page-link" href="#!" aria-label="Previous">
-											<span aria-hidden="true">&laquo;</span>
-										</a>
-									</li>
+								<li className="page-item" onClick={() => {
+									setPage(startPage - 1);
+									search();
+								}}>
+									<a className="page-link" href="#!" aria-label="Previous">
+										<span aria-hidden="true">&laquo;</span>
+									</a>
+								</li>
 							}
 							{
 								(startPage > 0 && endPage > 0) &&
-									Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i)
-										.map(i => (page === i)
-											? <li key={i} className="page-item active"><a className="page-link" href="#">{i}</a></li>
-											: <li key={i} className="page-item" onClick={() => searchPaging(i)}><a className="page-link" href="#">{i}</a></li>)
+								Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i)
+									.map(i => (page === i)
+										? <li key={i} className="page-item active"><a className="page-link" href="#">{i}</a></li>
+										: <li key={i} className="page-item" onClick={() => {
+											setPage(i);
+											search();
+										}}><a className="page-link" href="#">{i}</a></li>)
 							}
 							{
 								(totalPage > endPage) &&
-									<li className="page-item" onClick={() => searchPaging(endPage + 1)}>
-										<a className="page-link" href="#" aria-label="Next">
-											<span aria-hidden="true">&raquo;</span>
-										</a>
-									</li>
+								<li className="page-item" onClick={() => {
+									setPage(endPage + 1);
+									search();
+								}}>
+									<a className="page-link" href="#" aria-label="Next">
+										<span aria-hidden="true">&raquo;</span>
+									</a>
+								</li>
 							}
 						</ul>
 
@@ -265,10 +295,29 @@ export default function MemberList() {
 							<div className="row">
 								<div className="col-12 mb-3">
 									<label className="form-label subheading">Created Date</label>
+									<button className="btn btn-sm btn-primary text-white float-end" onClick={() => {
+											createdDateFromField.onChange(undefined);
+											createdDateToField.onChange(undefined);
+									}}>RESET</button>
 									<div className="input-group">
-										<input type="text" className="form-control" placeholder="Created Date From..." {...createdDateFromRegister} />
+										<DatePicker
+											wrapperClassName="form-control"
+											customInput={<input type="text" className="form-control" />}
+											dateFormat={DATE_TIME_FORMAT.DATE}
+											selected={createdDateFromField.value}
+											maxDate={createdDateToField.value}
+											onChange={(date) => createdDateFromField.onChange(date)}
+											disabledKeyboardNavigation={true}
+										/>
 										<span className="input-group-text">~</span>
-										<input type="text" className="form-control" placeholder="Created Date To..." {...createdDateToRegister} />
+										<DatePicker
+											wrapperClassName="form-control"
+											customInput={<input type="text" className="form-control" readOnly />}
+											dateFormat={DATE_TIME_FORMAT.DATE}
+											minDate={createdDateFromField.value}
+											selected={createdDateToField.value}
+											onChange={(date) => createdDateToField.onChange(date)}
+										/>
 									</div>
 								</div>
 							</div>
@@ -276,10 +325,28 @@ export default function MemberList() {
 							<div className="row">
 								<div className="col-12 mb-3">
 									<label className="form-label subheading">Updated Date</label>
+									<button className="btn btn-sm btn-primary text-white float-end" onClick={() => {
+											updatedDateFromField.onChange(undefined);
+											updatedDateToField.onChange(undefined);
+									}}>RESET</button>
 									<div className="input-group">
-										<input type="text" className="form-control" placeholder="Updated Date From..." {...updatedDateFromRegister} />
+										<DatePicker
+											wrapperClassName="form-control"
+											customInput={<input type="text" className="form-control" readOnly />}
+											dateFormat={DATE_TIME_FORMAT.DATE}
+											maxDate={updatedDateToField.value}
+											selected={updatedDateFromField.value}
+											onChange={(date) => updatedDateFromField.onChange(date)}
+										/>
 										<span className="input-group-text">~</span>
-										<input type="text" className="form-control" placeholder="Updated Date To..." {...updatedDateToRegister} />
+										<DatePicker
+											wrapperClassName="form-control"
+											customInput={<input type="text" className="form-control" readOnly />}
+											dateFormat={DATE_TIME_FORMAT.DATE}
+											minDate={updatedDateFromField.value}
+											selected={updatedDateToField.value}
+											onChange={(date) => updatedDateToField.onChange(date)}
+										/>
 									</div>
 								</div>
 							</div>
@@ -294,7 +361,7 @@ export default function MemberList() {
 										</select>
 										<span className="input-group-text">By</span>
 										<select className="form-select" {...orderRegister}>
-										<option value="">Choose...</option>
+											<option value="">Choose...</option>
 											{Object.entries(ORDER).map(([key, value]) => <option key={key} value={key}>{value}</option>)}
 										</select>
 									</div>
