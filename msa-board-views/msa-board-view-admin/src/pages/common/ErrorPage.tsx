6cb5ea5ba75196de456da81e3e00953e4f3fed0e
Kim Jong Young <jykim@saltlux.com>
2024-06-04 18:00:47 +0900

    feat: ERROR_CODE에 따른 에러 페이지 리다이렉트 추가

--- msa-board-views/msa-board-view-admin/src/pages/common/ErrorPage.tsx
+++ msa-board-views/msa-board-view-admin/src/pages/common/ErrorPage.tsx
@@ -1,9 +1,45 @@
+import { ERROR_CODE } from "msa-board-view-common/src/constants/constants";
+import * as sessionUtils from "msa-board-view-common/src/utils/sessionUtils";
+import { useEffect, useRef } from "react";
 import { FallbackProps } from "react-error-boundary";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 export default function ErrorPage({
 	error,
 	resetErrorBoundary
 }: FallbackProps) {
+
+	const navigate = useNavigate();
+	const errorCode = ("errorCode" in error) ? error.errorCode : null;
+	const errorMessage = ("errorCode" in error) ? error.errorMessage : null;
+
+	const { pathname } = useLocation();
+	const originalPathname = useRef(pathname);
+
+	useEffect(() => {
+		if (pathname !== originalPathname.current) {
+			resetErrorBoundary();
+		}
+	}, [pathname, resetErrorBoundary])
+
+	useEffect(() => {
+
+		switch (errorCode) {
+			case ERROR_CODE.EXPIRED_ACCESS_TOKEN:
+			case ERROR_CODE.EXPIRED_REFRESH_TOKEN:
+			case ERROR_CODE.REVOKED_ACCESS_TOKEN:
+			case ERROR_CODE.REVOKED_REFRESH_TOKEN:
+			case ERROR_CODE.INVALID_ACCESS_TOKEN:
+			case ERROR_CODE.INVALID_REFRESH_TOKEN:
+				sessionUtils.initSessionInfo();
+				alert(errorMessage);
+				navigate("/account/login/form");
+				resetErrorBoundary();
+				break;
+			default:
+				break;
+		}
+	}, []);
 
 	return (
 		// <!-- Error -->
